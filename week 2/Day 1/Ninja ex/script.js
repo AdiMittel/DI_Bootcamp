73f3b490028263689f576db6ae58c6d66b1bf4e4
AdiMittel <adidi2199@gmail.com>
2021-10-10 20:55:58 +0300

    i

---
+++ week 2/Day 1/Ninja ex/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//Exercise 1
+
+// let sentence = prompt('Give me a sentence with the word nemo and i will find id :)')
+
+// let whreNemo = sentence.indexOf('Nemo')
+// console.log('I found Nemo at:', whreNemo);
+
+//Exercise 3
+
+let nums =prompt('Enter 2 numbers seperated by , :')
+nums.replace(',' , ' ')
+let result = parseInt(nums)
+console.log(nums);
+
