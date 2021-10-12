6dc6f75fac098b7bec83c4a3370797acb3bf1485
AdiMittel <adidi2199@gmail.com>
2021-10-12 19:02:18 +0300

    i

---
+++ week-2/day-3/Challenge/script.js
@@ -0,0 +1,18 @@
+let astriks = "";
+
+for (let i = 0; i < 5; i++) {
+    astriks+='*'
+    console.log(astriks);
+}
+
+
+let astriks2 = "";
+
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < i; j++) {
+        astriks2+= '*'
+        
+        console.log(astriks2);
+    }
+    
+}
